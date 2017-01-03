da85866ea697fc08bf966a49798561ce8b9c8675
berurier <beruriervon77@gmail.com>
2017-01-03 14:08:39 +0100

    Update elf_reader.h

--- elf_reader.h
+++ elf_reader.h
@@ -3,3 +3,9 @@
 #include <byteswap.h>
 #include <string.h>
 #include <stdlib.h>
+
+ Elf32_Ehdr lectureheader(FILE* f);
+
+uint32_t Swap32(uint32_t valeur, char endianess);
+
+uint16_t Swap16(uint16_t valeur, char endianess);
