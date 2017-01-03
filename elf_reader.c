d0e32a93c5dbfb9090479ca37e3578a2f231c9eb
berurier <beruriervon77@gmail.com>
2017-01-03 13:23:39 +0100

    Update elf_reader.c

--- elf_reader.c
+++ elf_reader.c
@@ -3,20 +3,24 @@
 
 int main (int argc, char *argv[]) 
 {
-File *fich; 
-  /*Elf Header
-  #Elf Section Header*/
+File *fich; /* Fichier ELF qu'on va lire */
+Elf32_Ehdr head; /* Header ELF de taille Fixe */
+  
+  /*Elf Section Header*/
+  
+  /* Verification du nombre d'arguments */
 if (argc!=2){
   printf("Erreur Arguments");
   return 0;
 }
 
-if ((fich = fopen("argv[1]","r")) == NULL ){
+  /* ouverture du fichier */
+if ((fich = fopen("argv[1]","rb")) == NULL ){
  printf("Erreur ouverture fichier"); 
   return 0;
 }
 
-
+/* Lecture de l'header ELF au début du fichier */
 
 
 
