b7f3ae759cd89d3ddef169b35646ad06b456b97b
berurier <beruriervon77@gmail.com>
2017-01-03 14:10:01 +0100

    Create endian.c

---
+++ endian.c
@@ -0,0 +1,17 @@
+#include "elf_reader.h";
+
+uint32_t Swap32(uint32_t valeur, char endianess){
+	if(endianess == ELFDATA2LSB){
+		return valeur;
+	}else{
+		return __bswap_32(valeur);
+	}
+}
+
+uint16_t Swap16(uint16_t valeur, char endianess){
+	if(endianess == ELFDATA2LSB){
+		return valeur;
+	}else{
+		return __bswap_16(valeur);
+	}
+}
