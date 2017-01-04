ee8ca95fccec5be5d5bf7252765f71e2b1b7bbbf
Mathias Novel <novelm@F216-14.e-im2ag.ujf-grenoble.fr>
2017-01-04 15:07:56 +0100

    Correction orthographique mineure de elf_rechercheSection.c

--- elf_rechercheSection.c
+++ elf_rechercheSection.c
@@ -9,13 +9,13 @@ void afficheSectionByName(Elf32_Ehdr fileHeader, Elf32_Shdr *sections_headers, F
 	
 	int offsetNameTable = sections_headers[fileHeader.e_shstrndx].sh_offset; //On prend l'offset de la section contenant les noms de sections
 
-	//On parour chaque section jusqu'à trouver la bonne ou, toute les parcourir
+	//On parcour chaque section jusqu'à trouver la bonne ou, toute les parcourirs
 	while (!trouve && i < fileHeader.e_shnum) {
 		//On se place dans le fichier afin de retouver le nom de la section courante
 		currentSection = sections_headers[i];
 		fseek(elf, offsetNameTable + currentSection.sh_name, SEEK_SET); //offset nous ammène au début du stockage des noms, et sh_name contient l'index du nom de la section courante
 
-		//On récupère le nom de la section courrante
+		//On recupere le nom de la section courrante
 		int j =0;
 		while((courant = fgetc(elf))!= '\0'){
 			currentName[j] = courant;	
@@ -42,8 +42,7 @@ void afficheSectionByName(Elf32_Ehdr fileHeader, Elf32_Shdr *sections_headers, F
 
 
 void afficheSectionByNum(Elf32_Ehdr fileHeader, Elf32_Shdr *sections_headers, FILE* elf, int secNum) {
-
-	if (secNum < 0 || secNum > fileHeader.e_shnum) {
+	if (secNum < 0 || secNum > fileHeader.e_shnum-1) {
 		printf("Aucune section ne correspond à ce numero\n");
 	} else {
 		Elf32_Shdr currentSection = sections_headers[secNum];
@@ -54,13 +53,16 @@ void afficheSectionByNum(Elf32_Ehdr fileHeader, Elf32_Shdr *sections_headers, FI
 
 void afficheContenue(Elf32_Shdr currentSection, FILE* elf) {
 		fseek(elf, currentSection.sh_offset, SEEK_SET); //On se place au début du contenu de la section
+
 		uint16_t hex;
 		int sizeFileImage = currentSection.sh_size;
 		int address = currentSection.sh_addr;
-		//Jusqu'à la fin du fichier, on écrit octet par octet en hexadecimal
+
+		//Jusqu'à la fin du fichier, on ecrit octet par octet en hexadecimal
 		printf("0x%x ",address);
 		for (int i =1; i < (sizeFileImage+1); i++) { 
 			hex =(uint8_t) fgetc(elf); //On lit un octet
+			
 			if (hex <=15) { //Si l'octet est inférieur à 15 on ajoute 0 car sinon il ne l'affiche pas
 				printf("0");
 			}
