10a9a6f79737379ced336ada7e9d85061ea95eda
Mathias Novel <novelm@F213-09.e-im2ag.ujf-grenoble.fr>
2017-01-04 10:35:51 +0100

    table section v2

--- elf_sectionTable.c
+++ elf_sectionTable.c
@@ -60,7 +60,7 @@ void affichageTabsection(Elf32_Shdr *section_elf, Elf32_Ehdr header_elf, FILE* e
 	int i, string_size = 0;
 
 	printf("Section Headers:\n");
-	printf("[Nr]	Name			Type		Addr	Off	Size	ES	Flg	Lk	Inf	Al\n");
+	printf("[Nr]	Name			Type			Addr	Off	Size	ES	Flg	Lk	Inf	Al\n");
 
 
 	for(i=0; i<header_elf.e_shnum; i++)
@@ -86,86 +86,197 @@ void affichageTabsection(Elf32_Shdr *section_elf, Elf32_Ehdr header_elf, FILE* e
 		//affichage de la traducion des type de section
 		switch(section_elf[i].sh_type){
 			case SHT_NULL :
-				printf("NULL		");
+				printf("NULL			");
 				break;
 			case SHT_PROGBITS :
-				printf("PROGBITS	");
+				printf("PROGBITS		");
 				break;
 			case SHT_SYMTAB :
-				printf("SYMTAB		");
+				printf("SYMTAB			");
 				break;
 			case SHT_STRTAB :
-				printf("STRTAB		");
+				printf("STRTAB			");
 				break;
 			case SHT_RELA :
-				printf("RELA		");
+				printf("RELA			");
 				break;
 			case SHT_HASH :
-				printf("HASH		");
+				printf("HASH			");
 				break;
 			case SHT_DYNAMIC :
-				printf("DYNAMIC		");
+				printf("DYNAMIC			");
 				break;
 			case SHT_NOTE :
-				printf("NOTE		");
+				printf("NOTE			");
 				break;
 			case SHT_NOBITS :
-				printf("NOBITS		");
+				printf("NOBITS			");
 				break;
 			case SHT_REL :
-				printf("REL		");
+				printf("REL			");
 				break;
 			case SHT_SHLIB :
-				printf("SHLIB		");
+				printf("SHLIB			");
 				break;
 			case SHT_DYNSYM :
-				printf("DYNSYM		");
+				printf("DYNSYM			");
+				break;
+			case SHT_INIT_ARRAY :
+				printf("INIT_ARRAY		");
+				break;
+			case SHT_FINI_ARRAY :
+				printf("FINI_ARRAY		");
+				break;
+			case SHT_PREINIT_ARRAY :
+				printf("PREINIT_ARRAY 	");
+				break;
+			case SHT_GROUP :
+				printf("GROUP			");
+				break;
+			case SHT_SYMTAB_SHNDX :
+				printf("SYMTAB_SHNDX	");
+				break;
+			case SHT_NUM :
+				printf("NUM				");
+				break;
+			case SHT_ARM_ATTRIBUTES :
+				printf("ARM_ATTRIBUTES		");
+				break;
+			case SHT_ARM_EXIDX:
+				printf("ARM_EXIDX		");
+				break;
+			case SHT_ARM_PREEMPTMAP  :
+				printf("ARM_PREEMPTMAP	");
+				break;
+			case SHT_GNU_verdef :
+				printf("VERDEF			");
+				break;
+			case SHT_GNU_verneed :
+				printf("VERNEED 		");
+				break;
+			case SHT_GNU_versym :
+				printf("VERSYM			");
+				break;
+			case SHT_LOOS :
+				printf("LOOS			");
 				break;
 			case SHT_LOPROC :
-				printf("LOPROC		");
+				printf("LOPROC			");
 				break;
 			case SHT_HIPROC :
-				printf("HIPROC		");
+				printf("HIPROC			");
 				break;
 			case SHT_LOUSER :
-				printf("LOUSER		");
+				printf("LOUSER			");
 				break;
 			case SHT_HIUSER :
-				printf("HIUSER		");
+				printf("HIUSER			");
 				break;
 			default :
-				printf("Erreur		");
+				printf("Inconnu			");
 				break;
 
 		};
 		
 		//affichage addresse de début, l'endroit du fichier où sont les section, leurs taile en octet
-		printf("%d	", section_elf[i].sh_addr);
+		printf("%x	", section_elf[i].sh_addr);
 		printf("%x	", section_elf[i].sh_offset);
 		printf("%x	", section_elf[i].sh_size);
 		printf("%x	", section_elf[i].sh_entsize);
 
 
+
+
 		//affichage des drapeaux
+		int inconnu = 1;
 		// WRITE FLAG
 		if(section_elf[i].sh_flags & 0x1)
 		{
 			printf("W");
+			inconnu =0;
 		}
 		// ALLOC FLAG
 		if(section_elf[i].sh_flags & 0x2) 
 		{
 			printf("A");
+			inconnu =0;
 		}
 		// EXECINSTR
 		if(section_elf[i].sh_flags & 0x4)
 		{
 			printf("X");
+			inconnu =0;
 		}
-		// MASKPROC
-		if(section_elf[i].sh_flags & 0xf0000000)
+		// MERGE
+		if(section_elf[i].sh_flags & 0x10)
 		{
 			printf("M");
+			inconnu =0;
+		}
+		// STRINGS
+		if(section_elf[i].sh_flags & 0x20)
+		{
+			printf("S");
+			inconnu =0;
+		}
+		// INFO_LINK 
+		if(section_elf[i].sh_flags & 0x40)
+		{
+			printf("I");
+			inconnu =0;
+		}
+		// LINK_ORDER 
+		if(section_elf[i].sh_flags & 0x80)
+		{
+			printf("L");
+			inconnu =0;
+		}
+		// OS_NONCONFORMING 
+		if(section_elf[i].sh_flags & 0x100)
+		{
+			printf("O");
+			inconnu =0;
+			// MASKPROC
+			if(section_elf[i].sh_flags & 0xf0000000)
+			{
+				printf("p");
+			}
+			// MASKOS
+			if(section_elf[i].sh_flags & 0x0ff00000)
+			{
+				printf("o");
+			}			
+		}
+		// GROUP 
+		if(section_elf[i].sh_flags & 0x200)
+		{
+			printf("G");
+			inconnu =0;
+		}
+		// TLS  
+		if(section_elf[i].sh_flags & 0x400)
+		{
+			printf("T");
+			inconnu =0;
+		}
+		// ORDERED 
+		if(section_elf[i].sh_flags & 0x40000000)
+		{
+			printf("O");
+			inconnu =0;
+		}
+		// EXCLUDE  
+		if(section_elf[i].sh_flags & 0x80000000)
+		{
+			printf("E");
+			inconnu =0;
+		}
+		if (section_elf[i].sh_flags == 0)
+		{
+			inconnu =0;
+		}
+		if(inconnu) {
+			printf("x");
 		}
 
 		printf("	");
@@ -184,7 +295,7 @@ void affichageTabsection(Elf32_Shdr *section_elf, Elf32_Ehdr header_elf, FILE* e
 
 	//lègende des drapeaux
 	printf("Key to Flags:\n");
- 	printf("W (write), A (alloc), X (execute), M (merge), ? (unknown)");
+ 	printf("W (write), A (alloc), X (execute), M (merge), S (string), I (info link), L (link order), G (group), O (ordered), T (TLS), E (exclude), O (os non conforming) p (proc), o (os)x (unknown)");
 
 
 
