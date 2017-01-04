aabf5acd970503300cd7071f6acd1d06733f243c
Plinz <an.duquennoy@gmail.com>
2017-01-04 16:37:15 +0100

    ajout test helloworld

---
+++ fichierTest/test.s
@@ -0,0 +1,35 @@
+	.cpu arm7tdmi
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 18, 4
+	.file	"test.c"
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello, World!\000"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	ldr	r0, .L2
+	bl	puts
+	mov	r3, #0
+	mov	r0, r3
+	ldmfd	sp!, {fp, pc}
+.L3:
+	.align	2
+.L2:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.5.3"
