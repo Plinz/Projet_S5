87eedb739125d87b482fa7ccee45bb75ff1224f0
berurier <beruriervon77@gmail.com>
2017-01-05 15:46:43 +0100

    Update elf_relocate.c

--- elf_relocate.c
+++ elf_relocate.c
@@ -194,7 +194,7 @@ int affichage_relocation(Elf32_Sym* tabSymbole, Elf32_Ehdr *fileHeader, Elf32_Sh
 			for (int l = 0; l < nombres; l++) {
 				// get type
 				type = relType[ELF32_R_TYPE(rel[j].r_info)]; 
-				printf("%d     %x     %s       %x    \n",rel[j].r_offset, rel[j].r_info, type, tabSymbole[ELF32_R_SYM(rel[j].r_info)].st_value);
+				printf("%d     %x     %s       %x    \n",rel[j].r_offset, rel[j].r_info, type, tabSymbole[rel[j].r_info].st_value);
 				printf(" num %d \n",j);
 				j++;				
 			}			
