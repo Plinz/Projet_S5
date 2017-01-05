4c981e31b2939b2efaa0d8c5a800b740f7731109
berurier <beruriervon77@gmail.com>
2017-01-05 13:42:19 +0100

    Update elf_relocate.c

--- elf_relocate.c
+++ elf_relocate.c
@@ -86,7 +86,7 @@ int taillerel(Elf32_Ehdr *file_header, Elf32_Shdr *section_headers, Elf32_Rel* l
 int affichage_relocation(Elf32_Sym* tabSymbole, Elf32_Ehdr *fileHeader, Elf32_Shdr *sections_headers, FILE* elf) {
 	
 	Elf32_Rela* rela = malloc(sizeof(Elf32_Rela)) ;
-	Elf32_Rel* rel = malloc(sizeof(Elf32_rel));
+	Elf32_Rel* rel = malloc(sizeof(Elf32_Rel));
 	
 	
 	int trela = taillerela(fileHeader,section_headers,lesrela,elf);
