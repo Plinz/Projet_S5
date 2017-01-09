fc40122684187685da9ef927bddb40f11b6fe9dd
Mathias Novel <novelm@f203-13.e-im2ag.ujf-grenoble.fr>
2017-01-09 16:14:26 +0100

    Main fusion

--- elf_fusion.c
+++ elf_fusion.c
@@ -11,47 +11,60 @@ int main (int argc, char *argv[])
 	//Declaration pour la fusion de fichier
 	FichierElf fichierElf1;
 	FichierElf fichierElf2;
+	FichierElf fichierElfRes;
 
 	//Init fichier1
-	printf("0\n");
-	fichierElf1.fichierElf = fopen(argv[1],"r");
-	if (fichierElf1.fichierElf == NULL) {
-		printf("Erreur lors de l'ouverture du fichier1\n");
-		exit(1);
-	}
-	fichierElf1.header_elf = lectureheader(fichierElf1.fichierElf);
-	fichierElf1.sectionsTable = malloc(sizeof(Elf32_Shdr)*fichierElf1.header_elf.e_shnum);
-	fseek(fichierElf1.fichierElf, fichierElf1.header_elf.e_shoff, SEEK_SET);
-	lectureTableSection(fichierElf1.fichierElf, fichierElf1.header_elf,fichierElf1.sectionsTable);
-
-	printf("Décalage de %d pour la  table\n",fichierElf1.sectionsTable[fichierElf1.header_elf.e_shstrndx].sh_offset);
+		fichierElf1.fichierElf = fopen(argv[1],"r");
+		if (fichierElf1.fichierElf == NULL) {
+			printf("Erreur lors de l'ouverture du fichier1\n");
+			exit(1);
+		}
+		fichierElf1.header_elf = lectureheader(fichierElf1.fichierElf);
+		fichierElf1.sectionsTable = malloc(sizeof(Elf32_Shdr)*fichierElf1.header_elf.e_shnum);
+		fseek(fichierElf1.fichierElf, fichierElf1.header_elf.e_shoff, SEEK_SET);
+		lectureTableSection(fichierElf1.fichierElf, fichierElf1.header_elf,fichierElf1.sectionsTable);
 
 
-	printf("1\n");
-	//Init fichier1
-	fichierElf2.fichierElf = fopen(argv[2],"r");
-	if (fichierElf2.fichierElf == NULL) {
-		printf("Erreur lors de l'ouverture du fichier2\n");
-		exit(1);
-	}
-	fichierElf2.header_elf = lectureheader(fichierElf2.fichierElf);
-	fichierElf2.sectionsTable = malloc(sizeof(Elf32_Shdr)*fichierElf2.header_elf.e_shnum);
-	fseek(fichierElf2.fichierElf, fichierElf2.header_elf.e_shoff, SEEK_SET);
-	lectureTableSection(fichierElf2.fichierElf, fichierElf2.header_elf,fichierElf2.sectionsTable);
+		//Init fichier1
+		fichierElf2.fichierElf = fopen(argv[2],"r");
+		if (fichierElf2.fichierElf == NULL) {
+			printf("Erreur lors de l'ouverture du fichier2\n");
+			exit(1);
+		}
+		fichierElf2.header_elf = lectureheader(fichierElf2.fichierElf);
+		fichierElf2.sectionsTable = malloc(sizeof(Elf32_Shdr)*fichierElf2.header_elf.e_shnum);
+		fseek(fichierElf2.fichierElf, fichierElf2.header_elf.e_shoff, SEEK_SET);
+		lectureTableSection(fichierElf2.fichierElf, fichierElf2.header_elf,fichierElf2.sectionsTable);
 
-	printf("2\n");
-	FILE *fichierDest = fopen(argv[3],"w+");
-	if (fichierDest == NULL) {
-		printf("Erreur lors de l'ouverture du fichierDest\n");
-		exit(1);
-	}
-
-	printf("3\n");
-	fusion(&fichierElf1,&fichierElf2,fichierDest);
+		//Init fichierRes
+		fichierElfRes.fichierElf = fopen(argv[3],"w+");
+		if (fichierElfRes.fichierElf == NULL) {
+			printf("Erreur lors de l'ouverture du fichierDest\n");
+			exit(1);
+		}
+		fichierElfRes.sectionsTable = malloc((fichierElf1.header_elf.e_shnum + fichierElf2.header_elf.e_shnum)*sizeof(Elf32_Shdr));
 
 
-	fclose(fichierElf1.fichierElf);
-	fclose(fichierElf2.fichierElf);
-	fclose(fichierDest);
-	//Les free ???
+		fusion(&fichierElf1,&fichierElf2,&fichierElfRes);
+
+
+		printf("Phase de liberation de la memoire :\n");
+		printf("FichierElf1 : ");
+		free(fichierElf1.sectionsTable);
+		//free(fichierElf1.tabSymbole); //Pas encore alloué
+		//free(fichierElf1.tabSymboleDynamique); //Pas encore alloué
+		fclose(fichierElf1.fichierElf);
+		printf("OK\n");
+		printf("FichierElf2 : ");
+		free(fichierElf2.sectionsTable);
+		//free(fichierElf2.tabSymbole); //Pas encore alloué
+		//free(fichierElf2.tabSymboleDynamique); //Pas encore alloué
+		fclose(fichierElf2.fichierElf);
+		printf("OK\n");
+		printf("FichierElfRes : ");
+		free(fichierElfRes.sectionsTable);
+		//free(fichierElfRes.tabSymbole); //Pas encore alloué
+		//free(fichierElfRes.tabSymboleDynamique); //Pas encore alloué
+		fclose(fichierElfRes.fichierElf);
+		printf("OK\n");
 }
