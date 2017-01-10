40cb7d9c8075662af505c9c870b57fddbee96465
berurier <beruriervon77@gmail.com>
2017-01-10 17:51:46 +0100

    Ajout concatenation

--- elf_fusionrel.c
+++ elf_fusionrel.c
@@ -70,13 +70,80 @@ Reloctable* crea_rel_table (FichierElf * elfile) {
 
 
 
-/*
-Section RelFusion(fichierElf* elf1, fichierElf* elf2, Elf32_Shdr sectionHeader1, Elf32_Shdr sectionHeader2) {
-// Si deux sections ont le même nom : On concat
+
+Section RelFusion(fichierElf* oldelf1, fichierElf* oldelf2, fichierElf newelf, Reloctable reltab1, Reloctable reltab2, int nbnewsymbole) {
+// Si deux sections ont le même nom : On concatene tout ca.
+Elf32_Ehdr oldheader1 = oldelf1->header_elf;
+Elf32_Shdr* oldsection_headers1 = oldelf1->sectionsTable;
+Elf32_Rel* oldlesrel1 = oldelf1->tabRel;
+Elf32_Sym *oldsym1 = oldelf1->tabSymbole;
+Elf32_Ehdr oldheader2 = oldelf2->header_elf;
+Elf32_Shdr* oldsection_headers2 = oldelf2->sectionsTable;
+Elf32_Rel* oldlesrel2 = oldelf2->tabRel;
+Elf32_Sym *oldsym2 = oldelf2->tabSymbole;
+Elf32_Ehdr newheader = newelf->header_elf;
+Elf32_Shdr* newsection_headers = newelf->sectionsTable;
+Elf32_Rel* newlesrel = newelf->tabRel;
+Elf32_Sym *newsym = newelf->tabSymbole;
+
+int nombre1 = 0;
+int numsec1 = reltab1.indice_section;
+int nombre2 = 0;
+int numsec2 = reltab2.indice_section;
+int indicenew = 0
+nombre1 = oldsection_headers1[numsec1].sh_size / oldsection_headers1[numsec1].sh_entsize;
+nombre2 = oldsection_headers2[numsec2].sh_size / oldsection_headers2[numsec2].sh_entsize;
+int type = 0;
+int idsym = 0;
+
+int lastoff = 0;
+
+for (int i = 0; i<nombre1 ; i++ ) {
+// On s'occupe de la premiere table des relocations
+	
+	// On calcule le nouvel offset 
+	newlesrel[indicenew].r_offset = oldlesrel1[i].r_offset;
+
+	// On enregistre le type et l'ID précédent. 
+	type  = ELF32_R_TYPE(oldlesrel1[i].r_info);
+	idsym = ELF32_R_SYM(oldlesrel1[i].r_info);
+
+
+	// On mets le type & le nouvel id 
+	// A FAIRE
+
+	// On stocke le dernier offset
+	lastoff = newlesrel[indicenew].r_offset;
+
+	// On incremente le nouvel indice que prendront la prochaine table
+	indicenew ++;
+}	
+
+for (int i = 0; i<nombre2 ; i ++ ) {
+// On concatene la seconde table des relocations
+
+	// On calcule le nouvel offset
+	newlesrel[indicenew].r_offset = lastoff + oldsection_headers2[numsec2].sh_entsize;
+	
+
+
+	// On enregistre le type et l'ID précédent.
+	type  = ELF32_R_TYPE(oldlesrel2[i].r_info);
+	idsym = ELF32_R_SYM(oldlesrel2[i].r_info);
+
+	// On mets le type & le nouvel id
+	// A FAIRE
+
+	// On stocke le dernier offset
+	lastoff = newlesrel[indicenew].r_offset;
+
+	// On incremente le nouvel indice que prendront la prochaine table
+	indicenew ++;
+}
 
 }
 
-*/
+
 Section RelUpdate (FichierElf* oldelf, FichierElf* newelf, Reloctable reltab, int nbnewsymbole) {
 // Si une section a besoin d'être mise a jour suite à la fusion. 
 // Appel de type reloctable[i] ?? On va tester ça.
@@ -101,7 +168,8 @@ nombre = oldsection_headers[numsec].sh_size / oldsection_headers[numsec].sh_ents
 
 	for (int i = 0; i<nombre ; i++) {
 	// Calcul de l'offset de reimplementation
-		// tabRel.tablerel[i]. 
+	// On va dire que l'offset ne change pas quand on ne fusionne pas 2 tables de relocation
+		newlesrel[i].r_offset = oldlesrel[i].r_offset
 
 	// Calcul du numero de symbole a partir de la nouvelle table des symboles.
 	// Calcul du truc pour les sections. 
@@ -126,7 +194,7 @@ nombre = oldsection_headers[numsec].sh_size / oldsection_headers[numsec].sh_ents
 				break;
 
 			case (28) :
-				// R_ARM_CALL
+				// R_ARM_CALL -- PAS SUR DU TOUT CELUI LA
 				break;
 
 			default :
